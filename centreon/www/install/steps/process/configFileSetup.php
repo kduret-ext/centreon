30991cc49d96ebb8380ac9626e69bf0faedc7275
Paul Oureib <poureib@centreon.com>
2025-09-03 10:25:43 +0200

    fix variables

--- centreon/www/install/steps/process/configFileSetup.php
+++ centreon/www/install/steps/process/configFileSetup.php
@@ -84,7 +84,7 @@ $centreonConfPmFile = $centreonEtcPath . '/conf.pm';
 $contents = file_get_contents('../../var/configFilePmTemplate');
 $contents = str_replace(array_keys($macroReplacements), array_values($macroReplacements), $contents);
 file_put_contents($centreonConfPmFile, $contents);
-chmod($centreonConfFile, 0660);
+chmod($centreonConfPmFile, 0660);
 
 /**
  * Database configuration file
@@ -95,7 +95,7 @@ $contents = str_replace(array_keys($macroReplacements), array_values($macroRepla
 $oldMask = umask(0137);
 file_put_contents($gorgoneDatabaseFile, $contents);
 umask($oldMask);
-chmod($centreonConfFile, 0640);
+chmod($gorgoneDatabaseFile, 0640);
 
 /**
  * Gorgone API configuration file
